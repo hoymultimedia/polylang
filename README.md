cb5f90cefcbab177e4dd28de286def2a3f83c253
Vincent Klaiber <vincentklaiber@gmail.com>
2016-05-11 15:32:09 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Translator
+
+A translations helper for [WordPlate](https://wordplate.github.io/). Built with [Polylang](https://wordpress.org/plugins/polylang/).
+
+## Installation
+
+Require this package, with [Composer](https://getcomposer.org/), in the root directory of your project.
+
+```bash
+composer require wordplate/translator
+```
+
+Login to the WordPress administrator dashboard and active the [Polylang](https://wordpress.org/plugins/polylang/) plugin.
+
+## Usage
+
+Register the translations with the `register_translations` method.
+
+```php
+register_translations([
+    'general' => [
+        'general-language-english' => 'The english language',
+        'general-language-swedish' => 'The swedish language',
+    ],
+    'contact' => [
+        'contact-email' => 'Contact page email string',
+        'contact-mobile' => 'Contact page mobile string',
+        'contact-telephone' => 'Contact page telephone string',
+    ],
+]);
+```
+
+Then to fetch and print a translation you can use the `trans` method.
+
+```php
+echo trans('contact-email');
+```
+
+## License
+
+Translator is licensed under [The MIT License (MIT)](LICENSE).
---
+++ src/helpers.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of WordPlate.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use BadFunctionCallException;
+
+if (!function_exists('register_translations')) {
+    /**
+     * Register translations.
+     *
+     * @param array $groups
+     *
+     * @throws BadFunctionCallException
+     *
+     * @return void
+     */
+    function register_translations(array $groups) {
+        if (!function_exists('pll_register_string')) {
+            throw new BadFunctionCallException('Please active the Polylang plugin.');
+        }
+
+        foreach ($groups as $group => $translations) {
+            foreach ($translations as $key => $description) {
+                pll_register_string($description, $key, $group);
+            }
+        }
+    }
+}
+
+if (!function_exists('trans')) {
+    /**
+     * Get translations by their strings.
+     *
+     * @param string $key
+     *
+     * @return string
+     */
+    function trans($key)
+    {
+        if (!function_exists('pll__')) {
+            throw new BadFunctionCallException('Please active the Polylang plugin.');
+        }
+
+        return pll__($key);
+    }
+};
